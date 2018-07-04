c247f2a72bcbb5dca7b6c0cf6c4c01a2769b7a7a
Christoffer Green <christoffer.l.green@gmail.com>
2018-07-04 22:38:52 +0200

    Increase spawnrate as time goes on

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -6,11 +6,13 @@ public class EnemyManager : MonoBehaviour {
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
 
+    int increaseSpawnRate = 20;
 
     void Start() {
         InvokeRepeating("Spawn", spawnTime, spawnTime);
-    }
 
+        InvokeRepeating("IncreaseSpawnRate", increaseSpawnRate, increaseSpawnRate);
+    }
 
     void Spawn() {
         if(playerHealth.currentHealth <= 0f) {
@@ -18,7 +20,23 @@ public class EnemyManager : MonoBehaviour {
         }
 
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-
         Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+
+    void IncreaseSpawnRate() {
+
+        //Todo: May want to do a logarithmic curve here
+        var spawnIncrease = 0.5f;
+
+        CancelInvoke("Spawn");
+
+        // Limit so that spwnrate never goes below 0.5 seconds
+        if((spawnTime - spawnIncrease) < 0.5) {
+            spawnTime = 0.5f;
+        } else {
+            spawnTime -= spawnIncrease;
+        }
+        //Todo: Probably want to adjust for the last spawntime here instead of setting 0
+        InvokeRepeating("Spawn", 0, spawnTime);
     }
 }
